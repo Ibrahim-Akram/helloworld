476222f4ce97da2edd9eecfbb02ba9faea41137d
Ibrahim-Akram <111633300+Ibrahim-Akram@users.noreply.github.com>
2022-10-04 02:12:42 +0530

    1ex Java

---
+++ ex3.java
@@ -0,0 +1,15 @@
+
+public class ex3{
+	public static void main(String[] args) {
+		int mark =50;
+		System.out.println("My mark is " +mark);
+		
+		if(mark>=50) {
+			System.out.println("Pass");
+		} else {
+			System.out.println("Fail");
+		}
+		
+		
+	}
+}
