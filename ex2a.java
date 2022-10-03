09db7e795aae7b6e7ff996dcba663185326241b9
Ibrahim-Akram <111633300+Ibrahim-Akram@users.noreply.github.com>
2022-10-04 03:45:20 +0530

    2exa java

---
+++ ex2a.java
@@ -0,0 +1,16 @@
+public class ex2a{
+	public static void main(String[] args) {
+		int num=20;
+		switch(num) {
+		case 1: System.out.println("One");
+			break;
+		case 2: System.out.println("Two");
+		    break;
+		case 3: System.out.println("Three");    
+			break;
+		case 4: System.out.println("Four");
+		    break;
+		default: System.out.println("Other Number");
+		}
+	}
+}
