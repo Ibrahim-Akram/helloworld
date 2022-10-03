c24d965f0683c88228ab6e8e35832f300e18cdfd
Ibrahim-Akram <111633300+Ibrahim-Akram@users.noreply.github.com>
2022-10-04 02:32:48 +0530

    2ex Java

---
+++ ex1.java
@@ -0,0 +1,10 @@
+
+public class ex1 {
+
+	public static void main(String[] args) {
+		int ns=40;
+		String mia=(ns%2==0)? "Even Number":"Odd Number";
+        System.out.println(mia);
+	}
+    	  }
+      
